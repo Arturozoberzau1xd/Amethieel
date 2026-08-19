a16934469b81041cad984c8c07d3f1ec1b7a4635
ArturoZoberzau <ramirez.garturo222.1922@gmail.com>
2026-08-18 21:34:02 -0600

    v2 ig

--- src/config/contact.js
+++ src/config/contact.js
@@ -1,2 +1,2 @@
 export const INSTAGRAM_USERNAME = "amethieel";
-export const INSTAGRAM_CHAT_URL = `https://www.instagram.com/amethieel/`;
+export const INSTAGRAM_CHAT_URL = `https://ig.me/m/${INSTAGRAM_USERNAME}`;
