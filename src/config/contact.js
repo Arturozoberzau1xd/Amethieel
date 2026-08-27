198c9761a049d500c61cbee8021de98f0a762bee
ArturoZoberzau <ramirez.garturo222.1922@gmail.com>
2026-08-27 09:52:09 -0600

    number phone

--- src/config/contact.js
+++ src/config/contact.js
@@ -1,3 +1,3 @@
 //export const INSTAGRAM_USERNAME = "amethieel";
 //export const INSTAGRAM_CHAT_URL = `https://ig.me/m/${INSTAGRAM_USERNAME}`;
-export const WHATSAPP_NUMBER = "521234567890";
+export const WHATSAPP_NUMBER = "523316810923";
