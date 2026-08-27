70f19723e35c07232cb77fa8cbecef8187f23fd5
ArturoZoberzau <ramirez.garturo222.1922@gmail.com>
2026-08-27 17:32:37 -0600

    contact

--- src/config/contact.js
+++ src/config/contact.js
@@ -1,3 +1,3 @@
 //export const INSTAGRAM_USERNAME = "amethieel";
 //export const INSTAGRAM_CHAT_URL = `https://ig.me/m/${INSTAGRAM_USERNAME}`;
-export const WHATSAPP_NUMBER = "523316810923";
+export const WHATSAPP_NUMBER = "527712985037";
